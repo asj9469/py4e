7de309e44108fb38ca2c3bb64f2326b2ac516df5
asj9469 <ghs051965@gmail.com>
2024-01-13 03:55:17 -0500

    adjustment

--- LeetCode/26 - Remove Duplicate from Sorted Array.py
+++ LeetCode/26 - Remove Duplicate from Sorted Array.py
@@ -14,7 +14,6 @@
 
 ####################################################
 # My Initial Approach
-from typing import List
 
 def removeDuplicates(self, nums: List[int]) -> int:
     unique = dict()
@@ -40,6 +39,7 @@ def removeDuplicates(self, nums: List[int]) -> int:
 # NeetCode Optimized Approach
 # super simple - using two pointers to go through the array once each, left pointer only gets updated when unique value is copied over
 
+def removeDuplicates(self, nums: List[int]) -> int:
     l = 1
     for r in range(1, len(nums)):
         if nums[r] != nums[r - 1]:
