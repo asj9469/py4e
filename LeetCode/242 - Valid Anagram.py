9e6c01374cda2745d907d6642c7551c1158dd20c
asj9469 <ghs051965@gmail.com>
2024-01-29 18:08:41 -0500

    added Neetcode approach to 242

--- LeetCode/242 - Valid Anagram.py
+++ LeetCode/242 - Valid Anagram.py
@@ -32,8 +32,24 @@ class Solution:
             newCount[t[i]] = newCount.get(t[i], 0) + 1
 
         # go through ogCount and compare key, value with newCount
+        ## Neetcode: this part is unnecessary because in Python3, you can just compare if the dictionary is equal
         for key in ogCount.keys():
             if ogCount.get(key) != newCount.get(key):
                 return False
 
         return True
+
+####################################################
+# Neetcode Approach
+# difference: returns the boolean value if the dictionary is equal
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        countS, countT = {}, {}
+
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        return countS == countT #you can just compare this without another for loop
