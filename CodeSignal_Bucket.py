1c3128993e83273751992ca15e6f5b1796b1323b
asj9469 <ghs051965@gmail.com>
2024-03-08 20:34:01 -0500

    added medium questions and bucket problem

---
+++ CodeSignal_Bucket.py
@@ -0,0 +1,40 @@
+
+commands = [
+    "goto A",
+    "create fileA",
+    "create fileB",
+    "goto B",
+    "create fileA",
+    "create fileB",
+    "create fileC",
+    "goto A",
+    "create fileA",
+    "create fileB",
+    "goto B",
+    "create fileB",
+    "create fileD",
+    "create fileC",
+]
+
+currDir = ""
+entireDir = []
+newFile = ""
+
+for c in commands:
+    if c.startswith("goto"):
+        currDir = c[5:]
+        if not currDir in entireDir:
+            entireDir.append([currDir, set()])
+    else:
+        newFile = c[8:]
+        for i in range(len(entireDir)):
+            if entireDir[i][0] == currDir:
+                entireDir[i][1].add(newFile)
+
+most = 0
+name = ""
+for i in range(len(entireDir)):
+    if most < len(entireDir[i][1]):
+        name = entireDir[i][0]
+
+print(name)
---
+++ LeetCode/Medium/0049 - Group Anagrams.py
@@ -0,0 +1,31 @@
+# Given an array of strings strs, group the anagrams together. You can ####################################################return the answer in any order.
+# An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+#
+# Example 1:
+# Input: strs = ["eat","tea","tan","ate","nat","bat"]
+# Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+#
+# Example 2:
+# Input: strs = [""]
+# Output: [[""]]
+#
+# Example 3:
+# Input: strs = ["a"]
+# Output: [["a"]]
+
+####################################################
+# My Solution
+# My solution is more simple than neetcode
+# time complexity = O(n)
+# space complexity = O(n)
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # { anagram : [list of words] }
+        anagrams = {}
+
+        for word in strs:
+            tempKey = "".join(sorted(word))
+            anagrams[tempKey] = anagrams.get(tempKey, []) + [word]
+
+        return anagrams.values()
