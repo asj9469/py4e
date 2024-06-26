cef42924bd6ebbc0811ed2f60ecd1e3b4c2e2932
asj9469 <ghs051965@gmail.com>
2024-06-25 23:09:16 -0400

    added more to 1046 last stone weight

--- LeetCode/1 - Easy/1046 - Last Stone Weight.py
+++ LeetCode/1 - Easy/1046 - Last Stone Weight.py
@@ -1,11 +1,47 @@
 # https://leetcode.com/problems/last-stone-weight/description/
 
-# My initial approach
-# time complexity: O(n log n)
-# space complexity: O(n)
+
 
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
+
+        # max() method
+        # time complexity: O(n^2) -> max function is O(n)
+        # space complexity: O(1)
+
+        while len(stones) > 1:
+            big1 = max(stones)
+            stones.pop(stones.index(big1))
+
+            big2 = max(stones)
+            stones.pop(stones.index(big2))
+
+            print(stones)
+            if big1 != big2:
+                stones.append(big1 - big2)
+
+        return stones[0] if stones else 0
+
+        # [review] heap method - 6/25/24
+        # time complexity: O(n log n)
+        # space complexity: O(n)
+
+        newStones = [-i for i in stones]
+        heapq.heapify(newStones)
+
+        while len(newStones) > 1:
+            big1 = -heapq.heappop(newStones)
+            big2 = -heapq.heappop(newStones)
+
+            if big1 != big2:
+                heapq.heappush(newStones, -(big1-big2))
+
+        return -newStones[0] if newStones else 0
+
+###############################################################
+        # first attempt with heap - 6/9/24
+        # time complexity: O(n log n)
+        # space complexity: O(n)
 
         if len(stones) == 1:
             return stones[0]
