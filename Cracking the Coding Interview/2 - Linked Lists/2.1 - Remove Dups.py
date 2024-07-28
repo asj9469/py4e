047312047c6c59cd7072da43fd46a3656225125e
asj9469 <ghs051965@gmail.com>
2024-07-28 00:01:29 -0400

    added cracking the coding interview dir

---
+++ Cracking the Coding Interview/2 - Linked Lists/2.1 - Remove Dups.py
@@ -0,0 +1,38 @@
+# write code to remove duplicates from an unsorted linked list
+# FOLLOW UP: how would you solve this problem if a temporary buffer (extra space) is not allowed?
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    # hashmap method
+    # runtime complexity: O(n)
+    # space complexity: O(n)
+    def deleteNodes(self, head: ListNode):
+        hash = set()
+        prev = None
+
+        while head:
+            if head.val in hash:
+                prev.next = head.next
+            else:
+                hash.add(head.val)
+                prev = head
+            head = head.next
+
+    # runner method (no extra space)
+    # for each element, go through the rest of the linked list to check if there are any duplicates
+    # runtime complexity: O(n^2)
+    # space complexity: O(1)
+    def deleteNodes2(self, head: ListNode):
+        curr = head
+        while curr:
+            runner = curr
+            while runner.next:
+                if runner.next.val == curr.val:
+                    runner.next = runner.next.next
+                else:
+                    runner = runner.next
+                curr = curr.next
