9882a9c431f626abc6907e81a9d75ad88e3e3584
asj9469 <ghs051965@gmail.com>
2024-09-19 16:33:10 -0400

    added 53 maximum subarray

---
+++ LeetCode/1-Arrays/53 - Maximum Subarray.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/maximum-subarray/
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        # runtime complexity: O(n)
+        # space complexity: O(1)
+
+        maxSum, currSum = nums[0], nums[0]
+
+        for i in range(1, len(nums)):
+            currSum = max(currSum + nums[i], nums[i])
+            maxSum = max(currSum, maxSum)
+
+        return maxSum
